c7dd2a29e4ab3d613a5df1ff6a997c2ece3b74e1
Tom Groot <4732713+Tomgroot@users.noreply.github.com>
2024-01-24 10:45:06 +0100

    Feat: let the user allow the redirect

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason === "install" || details.reason === "update") {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            if (tabs.length > 0) {
+                // If an active tab is found, update its URL
+                chrome.tabs.update(tabs[0].id, {url: "clickup://home"});
+            } else {
+                // If no active tab is found, you might want to open a new tab or handle this case differently
+                console.log("No active tab found");
+            }
+        });
+    }
+});
+
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if(request.redirect) {
         chrome.tabs.update(sender.tab.id, {url: request.redirect}, function() {
