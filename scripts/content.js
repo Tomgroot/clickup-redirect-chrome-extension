4fb5c65ec3e17d1a4c134c5d02c5dd9ba3a0c29f
Tom Groot <tom@eventix.nl>
2024-12-20 11:51:08 +0100

    feat: add disable button

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,10 @@
 var currentURL = window.location.href;
-if(currentURL.includes("https://app.clickup.com")) {
+let disabled = false;
+
+chrome.storage.local.get('disabled', function(data) {
+    disabled = data.disabled;
+});
+
+if(!disabled && currentURL.includes("https://app.clickup.com")) {
     chrome.runtime.sendMessage({redirect: currentURL.replace('https://app.clickup.com', 'clickup://')});
 }
